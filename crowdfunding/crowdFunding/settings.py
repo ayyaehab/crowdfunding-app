92069c6f4bac051a007c4a435189a8a392cb25f8
ayyaehab <76024119+ayyaehab@users.noreply.github.com>
2021-12-05 23:13:10 +0200

    Update settings.py

--- crowdfunding/crowdFunding/settings.py
+++ crowdfunding/crowdFunding/settings.py
@@ -151,8 +151,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'haikalaya28@gmail.com'
-EMAIL_HOST_PASSWORD = 'aya12345'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
 
